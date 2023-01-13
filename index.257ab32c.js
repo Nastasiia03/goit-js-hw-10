2d0445cf6e393462bc58326af109591a3e12f31d
Nastasiia03 <110775841+Nastasiia03@users.noreply.github.com>
2023-01-13 23:30:11 +0000

    Deploying to gh-pages from @ Nastasiia03/goit-js-hw-10@512cb836d5aaa7f696eef23b91bc00ca4ec92e64 🚀

--- index.257ab32c.js
+++
@@ -1,2 +0,0 @@
-var t;fetch(`https://restcountries.com/v3.1/name/${t}?fields=name,capital,population,flags,languages`).then((t=>{if(!t.ok)throw new Error(t.statusText);return t.json()}));
-//# sourceMappingURL=index.257ab32c.js.map
--- index.ae69af3c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t;fetch("".concat("https://restcountries.com/v3.1/name/").concat(t,"?fields=name,capital,population,flags,languages")).then((function(t){if(!t.ok)throw new Error(t.statusText);return t.json()}))}();
-//# sourceMappingURL=index.ae69af3c.js.map
